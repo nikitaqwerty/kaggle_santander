24f82785c7be51baa1ccecff142eb969b2113e01
Nikita <nikitaqwerty4@gmail.com>
2019-04-02 19:10:06 +0300

    nn script fix

--- scripts/nn.py
+++ scripts/nn.py
@@ -7,17 +7,16 @@ import random
 import warnings
 
 from tqdm import tqdm
-from sklearn.preprocessing import scale
 from sklearn.metrics import roc_auc_score
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import StratifiedKFold
 
 from torch.optim import Adam
-from torch.optim.lr_scheduler import MultiStepLR
-from torch.nn.modules.loss import BCEWithLogitsLoss, BCELoss
-from torch.utils.data import TensorDataset, Dataset, DataLoader
+from torch.optim.lr_scheduler import ReduceLROnPlateau
+from torch.nn.modules.loss import BCEWithLogitsLoss
 
 warnings.filterwarnings("ignore")
+
 
 def feature_generator(df, vcs_train_test):
     for i in tqdm(range(200)):
@@ -28,6 +27,18 @@ def feature_generator(df, vcs_train_test):
 
         df[col + '_train_test_sum_vcs'] = t
         df[col + '_train_test_sum_vcs_product'] = df[col] * t
+
+
+def batch_iter(X, y, batch_size=64):
+    """
+    X: feature tensor (shape: num_instances x num_features)
+    y: target tensor (shape: num_instances)
+    """
+    idxs = torch.randperm(X.size(0))
+    if X.is_cuda:
+        idxs = idxs.cuda()
+    for batch_idxs in idxs.split(batch_size):
+        yield X[batch_idxs], y[batch_idxs]
 
 
 class NN(torch.nn.Module):
@@ -56,8 +67,8 @@ class NN(torch.nn.Module):
         y = self.linear3(y)
         return y
 
-def main():
 
+def main():
     random_seed = 42
     np.random.seed(random_seed)
     random.seed(random_seed)
@@ -85,7 +96,7 @@ def main():
 
     vcs_train_test = {}
 
-    for col in tqdm(train.columns):
+    for col in train.columns:
         vcs_train_test[col] = train_test.loc[:, col].value_counts()
 
     feature_generator(train, vcs_train_test)
@@ -94,11 +105,11 @@ def main():
     scaler = StandardScaler()
     cols = train.columns
 
-    X_train = pd.DataFrame(scaler.fit_transform(train),columns=cols)
-    test = pd.DataFrame(scaler.transform(test),columns=cols)
+    X_train = pd.DataFrame(scaler.fit_transform(train), columns=cols)
+    test = pd.DataFrame(scaler.transform(test), columns=cols)
 
     loss_f = BCEWithLogitsLoss()
-    batch_size = 2048
+    batch_size = 16384
     device = torch.device('cuda:3')
 
     skf = StratifiedKFold(n_splits=5, shuffle=True, random_state=42)
@@ -113,9 +124,9 @@ def main():
         test_tensors = []
 
         for fff in range(200):
-            train_t = X_train.loc[trn_idx,[f'var_{fff}',
-                                           f'var_{fff}_train_test_sum_vcs',
-                                           f'var_{fff}_train_test_sum_vcs_product']].values
+            train_t = X_train.loc[trn_idx, [f'var_{fff}',
+                                            f'var_{fff}_train_test_sum_vcs',
+                                            f'var_{fff}_train_test_sum_vcs_product']].values
             val_t = X_train.loc[val_idx, [f'var_{fff}',
                                           f'var_{fff}_train_test_sum_vcs',
                                           f'var_{fff}_train_test_sum_vcs_product']].values
@@ -139,18 +150,14 @@ def main():
         y_val_t = torch.tensor(label[val_idx].values, requires_grad=False,
                                device=device, dtype=torch.float32)
 
-        dataset = TensorDataset(train_tensors, y_train_t)
         nn = NN().to(device)
         optimizer = Adam(params=nn.parameters(), lr=0.005)
-        scheduler = MultiStepLR(optimizer, milestones=[15, 25, 35, 55], gamma=0.5)
+        scheduler = ReduceLROnPlateau(optimizer, 'max', factor=0.5, patience=5, min_lr=0.0001, verbose=True)
         best_AUC = 0
         early_stop = 0
 
-        for epoch in tqdm(range(100)):
-            dl = DataLoader(dataset,
-                            batch_size=batch_size,
-                            shuffle=True,
-                            num_workers=0)
+        for epoch in tqdm(range(200)):
+            dl = batch_iter(train_tensors, y_train_t, batch_size=batch_size)
             for data, label_t in dl:
                 pred = nn(data)
                 loss = loss_f(pred, torch.unsqueeze(label_t, -1))
@@ -163,40 +170,40 @@ def main():
                 print('EPOCH {}'.format(epoch))
                 print('LOSS: ', loss_f(val_pred, torch.unsqueeze(y_val_t, -1)).detach().cpu().numpy())
                 print('AUC: ', AUC)
-                print('=' * 50)
-                scheduler.step()
+                scheduler.step(AUC)
 
                 if AUC > best_AUC:
                     early_stop = 0
                     best_AUC = AUC
-                    torch.save(nn, output_path + 'best_auc_nn.pkl')
+                    torch.save(nn, 'best_auc_nn.pkl')
                 else:
-                    print('SCORE IS NOT THE BEST. Early stop counter: {}'.format(early_stop))
                     early_stop += 1
+                    print('SCORE IS NOT THE BEST. Early stop counter: {}'.format(early_stop))
 
                 if early_stop == 15:
                     print('EARLY_STOPPING')
-                    best_model = torch.load(output_path + 'best_auc_nn.pkl')
                     break
+                print('=' * 50)
+
+            best_model = torch.load('best_auc_nn.pkl')
 
         with torch.no_grad():
             oof[val_idx] = best_model(val_tensors).data.cpu().numpy().flatten()
 
-            batch_size = 20000
             blobs = []
-
-            for batch in torch.split(test_tensors, batch_size):
+            for batch in torch.split(test_tensors, 20000):
                 blob = best_model(batch).data.cpu().numpy().flatten()
                 blobs.append(blob)
         predictions_test = np.concatenate(blobs)
 
-        predictions += predictions_test / folds.n_splits
+        predictions += predictions_test / skf.n_splits
 
     auc = round(roc_auc_score(label, oof), 5)
     print("CV score: {:<8.5f}".format(auc))
 
     np.save(output_path + f"nn_{auc}_oof.npy", oof)
     np.save(output_path + f"nn_{auc}_test.npy", predictions)
+
 
 if __name__ == "__main__":
     main()
